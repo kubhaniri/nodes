f39e45c77434b09c96e4677d909cc8434015ba5d
kubha <firmsong@fn3s.com>
2020-01-29 14:09:45 +0100

    eslint and prettier

--- src/index.js
+++ src/index.js
@@ -3,62 +3,35 @@ const express = require('express');
 var module_http= require('http');
 var module_fs= require('fs');
 var module_url= require('url');
-var module_querystring= require('querystring');*/
-const mymod = require('./module/mymod.js')
-const {countries} = require('countries-list')
+var module_querystring= require('querystring'); */
+const { info, err } = require('./module/mymod.js');
+const { countries, languages } = require('countries-list');
 
 const app = express();
-app.get('/', function(require, response){
-    response.status('200').send("Firmin Est Present");
+app.get('/', (request, response) => {
+  response.status('200').send('Firmin Est Present');
 });
 
-app.get('/info', function(request,response){
-    response.send(mymod.info0("Firmin's Info"));
+app.get('/info', (request, response) => {
+  response.send(info("Firmin's Info"));
 });
 
-app.get('*', function(request, response){
-    response.status('404').send("Firmin Est En Mode Invisible");
-});
-/*
-
-var server = module_http.createServer(function(request, response){
-
-    var parsed_url = module_url.parse(request.url);
-    var pathname = parsed_url.pathname;
-    var query = module_querystring.parse(parsed_url.query);
-    console.log("query is : ", query);
-
-    if(pathname==="/"){
-        response.writeHead(200, {"Content-Type":"text/html"});
-        response.write('<html><body><p>HELLO</p></body></html>');
-    }else 
-    if(pathname==="/exit"){ 
-        response.writeHead(200, {"Content-Type":"text/html"});
-        response.write('<html><body><p>BYE</p></body></html>');
-    }else 
-    if(pathname==="/info"){ 
-        var result = mymod.info(pathname)
-        response.writeHead(200, {"Content-Type":"text/html"});
-        response.write(result);
-    }else 
-    if(pathname==="/country"){ 
-        response.writeHead(200, {"Content-Type":"application/json"});
-        response.write(JSON.stringify(countries[query.code]));
-    }else 
-    if(pathname==="/error"){ 
-        var result = mymod.err(pathname)
-        response.writeHead(200, {"Content-Type":"text/html"});
-        response.write(result);
-    }else { 
-        response.writeHead(404, {"Content-Type":"text/html"});
-        response.write('<html><body><p>NOT FOUND</p></body></html>');
-    }
-response.end();
+app.get('/country', (request, response) => {
+  console.log('request.query :', request.query);
+  response.json(countries[request.query.code]);
+  //response.send(JSON.stringify(countries[request.query.code]));
 });
 
-*/
+app.get('/languages/:lang', (request, response) => {
+  console.log('request.params  ', request.params);
+  response.json(languages[request.params.lang]);
+  //response.send(JSON.stringify(countries[request.query.code]));
+});
 
-app.listen(4000, function(){
-    console.log("Running on Port:4000");
-}
-);
+app.get('*', (request, response) => {
+  response.status('404').send('Firmin Est En Mode Invisible');
+});
+
+app.listen(4000, () => {
+  console.log('Running on Port:4000');
+});
--- src/module/mymod.js
+++ src/module/mymod.js
@@ -1,11 +1,12 @@
-module.exports.info0 = function info(txt){
-    console.log("My text is:", txt);
-    return txt;
-}
+const info = txt => {
+  console.log('My text is:', txt);
+  return txt;
+};
 
-module.exports.err0 = function err(txt){
-    console.log("ERROR :", txt);
-    return txt;
-}
+const err = txt => {
+  console.log('ERROR :', txt);
+  return txt;
+};
 
-//module.exports = {info, err};
+module.exports.info = info;
+module.exports.err = err;
