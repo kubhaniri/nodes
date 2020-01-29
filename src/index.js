bac1d1a8db01da345b87c494a263bacf097375c0
kubha <firmsong@fn3s.com>
2020-01-29 17:21:05 +0100

    defining route

--- src/index.js
+++ src/index.js
@@ -1,39 +1,10 @@
 const express = require('express');
 
-const { countries, languages } = require('countries-list');
-const { info } = require('./module/mymod.js');
+const routes = require('./routes');
 
 const app = express();
-app.get('/', (request, response) => {
-  response.status('200').send('Firmin Est Present');
-});
 
-app.get('/info', (request, response) => {
-  response.send(info("Firmin's Info"));
-});
-
-app.get('/country', (request, response) => {
-  //console.log('request.query :', request.query);
-  response.json(countries[request.query.code]);
-  //response.send(JSON.stringify(countries[request.query.code]));
-});
-
-app.get('/languages/:lang/:gh', (request, response) => {
-  //console.log('request.params  ', request.params);
-  const lang = languages[request.params.lang];
-  if (lang) {
-    response.json({ status: 'OK', message: lang });
-  } else {
-    response.status(404).json({
-      status: 'NOT_FOUND',
-      message: `language${request.params.lang}not found`
-    });
-  }
-});
-
-app.get('*', (request, response) => {
-  response.status('404').send('Firmin Est En Mode Invisible');
-});
+routes(app);
 
 app.listen(4000, () => {
   console.log('Running on Port:4000');
