0f84496139d7fba82887b313f692f5859f4c5875
kubha <firmsong@fn3s.com>
2020-01-31 00:37:46 +0100

    conex string mongodb

--- src/controllers/V1/users-controller.js
+++ src/controllers/V1/users-controller.js
@@ -1,14 +1,19 @@
 const createUser = (req, res) => {
+  console.log('req.body', req.body);
+
   res.send({ status: 'OK', message: 'User Created' });
 };
 
-const deleteUser = (req, res) => {res.send({ status: 'OK', message: 'User deleted '});
+const deleteUser = (req, res) => {
+  res.send({ status: 'OK', message: 'User deleted ' });
 };
 
-const updateUser = (req, res) => {res.send({ status: 'OK', message: 'User updated '});
+const updateUser = (req, res) => {
+  res.send({ status: 'OK', message: 'User updated ' });
 };
 
-const getUsers = (req, res) => {res.send({ status: 'OK', message: 'Users got '});
+const getUsers = (req, res) => {
+  res.send({ status: 'OK', message: 'Users got ' });
 };
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 const routesV1 = require('./routes/V1');
 
+dotenv.config();
+
 const app = express();
+
+console.log('DB connexion : ', process.env.MONGO);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 routesV1(app);
 
