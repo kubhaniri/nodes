ac23b37790c9484c1f098ded5f5c98cae7e10ff0
kubha <firmsong@fn3s.com>
2020-02-02 17:10:02 +0100

    security

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,16 @@
+const isValidHostname = (req, res, next) => {
+  const validHost = ['localhost', 'fsong.cm'];
+
+  if (validHost.includes(req.hostname)) {
+    next();
+  } else {
+    res.status(403).send({ status: 'ACCESS_DENIED' });
+  }
+};
+
 const isAuth = (req, res, next) => {
-    
+  console.log('req.headers', req.headers);
   next();
 };
 
-module.exports = { isAuth };
+module.exports = { isAuth, isValidHostname };
--- src/routes/V1/users-route.js
+++ src/routes/V1/users-route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const usersController = require('../../controllers/V1/users-controller');
-const { isAuth } = require('../../middlewares/auth');
+const { isAuth, isValidHostname } = require('../../middlewares/auth');
 
 const router = express.Router();
 
 router.post('/login', usersController.login);
 router.post('/create', usersController.createUser);
-router.post('/update', isAuth, usersController.updateUser);
+router.post('/update', isValidHostname, isAuth, usersController.updateUser);
 router.post('/delete', usersController.deleteUser);
 router.get('/get-all', usersController.getUsers);
 
